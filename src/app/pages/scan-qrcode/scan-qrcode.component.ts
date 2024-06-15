181c3226bbe3e45ebfaa28d114af93a296e86bc5
seremwen <seremwe.81@gmail.com>
2024-06-15 02:10:47 +0100

    this.qrResultString = data;

--- src/app/pages/scan-qrcode/scan-qrcode.component.ts
+++ src/app/pages/scan-qrcode/scan-qrcode.component.ts
@@ -119,12 +119,13 @@ src: any;
   }
 
   onCodeResult(data: string) {
+    this.qrResultString = data;
     if (data) {
       console.log('Scanned data:', data); // Log the initial scanned data
       this.contentOf=data
       console.log('Data type:', typeof data); // Log the type of the data
       console.log('Data length:', data.length); // Log the length of the data
-
+   
       // If data is base64 encoded, decode it
       const isBase64 = /^[A-Za-z0-9+/=]+$/.test(data);
       const binaryData = isBase64 ? atob(data) : data;
